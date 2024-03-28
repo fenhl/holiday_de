f5d335196290b7d0d9945a3c70294b8ff3980ea2
Fenhl <fenhl@fenhl.net>
2024-03-28 13:33:05 +0000

    Split regions based on holiday rules; Add missing changes between 1995 and today

--- src/regions.rs
+++ src/regions.rs
@@ -7,29 +7,31 @@ use chrono::{Datelike, NaiveDate};
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum GermanRegion {
     BadenWuerttemberg,
-    /// * The Augsburger Friedensfest only applies to Augsburg.
-    ///   It is excluded by default, but can be calculated via `GermanHoliday::AugsburgerFriedensfest`.
-    /// * Mariä Himmelfahrt only applies to communities with a catholic majority.
-    ///   Since this is the case in the majority of communities, it is included by default.
-    Bayern,
+    /// Communities of Bavaria with Protestant majorities.
+    BayernEv,
+    /// Communities of Bavaria with Catholic majorities, not including Augsburg.
+    BayernKath,
+    Augsburg,
     Berlin,
     Brandenburg,
     Bremen,
     Hamburg,
     Hessen,
-    MechlenburgVorpommern,
+    MecklenburgVorpommern,
     Niedersachsen,
     NordrheinWestfalen,
     RheinlandPfalz,
     Saarland,
-    /// Fronleichnam applies only to a minority of communities and has been excluded by default.
-    /// It can be manually calculated via `GermanHoliday::Fronleichnam`.
-    Sachsen,
+    /// Parts of Saxony where Fronleichnam is not a public holiday.
+    SachsenOhneFronleichnam,
+    /// Parts of Saxony where Fronleichnam is a public holiday.
+    SachsenMitFronleichnam,
     SachsenAnhalt,
     SchleswigHolstein,
-    /// Fronleichnam applies only to a minority of communities and has been excluded by default.
-    /// It can be manually calculated via `GermanHoliday::Fronleichnam`.
-    Thueringen,
+    /// Parts of Thuringia where Fronleichnam is not a public holiday.
+    ThueringenOhneFronleichnam,
+    /// Parts of Thuringia where Fronleichnam is a public holiday.
+    ThueringenMitFronleichnam,
 }
 
 use crate::holidays::GermanHoliday;
@@ -48,15 +50,39 @@ impl GermanRegion {
         let mut holidays = Vec::new();
         holidays.extend_from_slice(BUNDESWEITE_FEIERTAGE);
         holidays.extend_from_slice(self.region_specific_holidays(year));
+        if year == 2017 && !holidays.contains(&Reformationstag) {
+            // BW: https://www.landesrecht-bw.de/perma?d=jlr-FeiertGBWV1P1a
+            // BY: https://www.bayern.landtag.de/www/ElanTextAblage_WP17/Drucksachen/Folgedrucksachen/0000007000/0000007463.pdf
+            // BE: https://gesetze.berlin.de/bsbe/document/aiz-jlr-FeiertGBErahmen%4020151025/part/x
+            // HE: https://www.rv.hessenrecht.hessen.de/bshe/document/jlr-RefT2017VHErahmen/part/X
+            // NW: https://www.landtag.nrw.de/portal/WWW/dokumentenarchiv/Dokument?Id=XMMGVB1528%7C496%7C496
+            // RP: https://web.archive.org/web/20160305005630/https://www.rlp.de/fr/aktuelles/einzelansicht/news/detail/News/zusaetzlicher-feiertag-2017/
+            // SL: https://web.archive.org/web/20160306062414/http://sl.juris.de/cgi-bin/landesrecht.py?d=http%3A%2F%2Fsl.juris.de%2Fsl%2Fgesamt%2FRefT2017V_SL.htm
+            holidays.push(Reformationstag);
+        }
         holidays
     }
 
     fn region_specific_holidays(&self, year: i32) -> &'static [GermanHoliday] {
         match self {
             BadenWuerttemberg => &[HeiligeDreiKoenige, Fronleichnam, Allerheiligen],
-            Bayern => &[
+            BayernEv => &[
                 HeiligeDreiKoenige,
                 Fronleichnam,
+                Allerheiligen,
+            ],
+            BayernKath => &[
+                HeiligeDreiKoenige,
+                Fronleichnam,
+                MariaeHimmelfahrt,
+                Allerheiligen,
+            ],
+            Augsburg => &[
+                HeiligeDreiKoenige,
+                Fronleichnam,
+                AugsburgerFriedensfest,
+                // https://statistik.bayern.de/statistik/gebiet_bevoelkerung/zensus/himmelfahrt/index.php
+                // https://www.zensus2022.de/DE/Aktuelles/Zensus_2022_Ergebnisveroeffentlichung_verschiebt_sich_in_Sommer_2024.html
                 MariaeHimmelfahrt,
                 Allerheiligen,
             ],
@@ -68,18 +94,62 @@ impl GermanRegion {
                 }
             }
             Brandenburg => &[Reformationstag],
-            Bremen => &[Reformationstag],
-            Hamburg => &[Reformationstag],
+            Bremen => {
+                if year >= 2017 {
+                    &[Reformationstag]
+                } else {
+                    &[]
+                }
+            }
+            Hamburg => {
+                if year >= 2017 {
+                    &[Reformationstag]
+                } else {
+                    &[]
+                }
+            }
             Hessen => &[Fronleichnam],
-            MechlenburgVorpommern => &[Reformationstag],
-            Niedersachsen => &[Reformationstag],
+            MecklenburgVorpommern => {
+                if year >= 2023 {
+                    &[Frauentag, Reformationstag]
+                } else {
+                    &[Reformationstag]
+                }
+            }
+            Niedersachsen => {
+                if year >= 2017 {
+                    &[Reformationstag]
+                } else {
+                    &[]
+                }
+            }
             NordrheinWestfalen => &[Fronleichnam, Allerheiligen],
             RheinlandPfalz => &[Fronleichnam, Allerheiligen],
             Saarland => &[Fronleichnam, MariaeHimmelfahrt, Allerheiligen],
-            Sachsen => &[Reformationstag, BussUndBettag],
+            SachsenOhneFronleichnam => &[Reformationstag, BussUndBettag],
+            SachsenMitFronleichnam => &[Fronleichnam, Reformationstag, BussUndBettag],
             SachsenAnhalt => &[HeiligeDreiKoenige, Reformationstag],
-            SchleswigHolstein => &[Reformationstag],
-            Thueringen => &[Weltkindertag, Reformationstag],
+            SchleswigHolstein => {
+                if year >= 2017 {
+                    &[Reformationstag]
+                } else {
+                    &[]
+                }
+            }
+            ThueringenOhneFronleichnam => {
+                if year >= 2019 {
+                    &[Weltkindertag, Reformationstag]
+                } else {
+                    &[Reformationstag]
+                }
+            }
+            ThueringenMitFronleichnam => {
+                if year >= 2019 {
+                    &[Fronleichnam, Reformationstag]
+                } else {
+                    &[Fronleichnam, Weltkindertag, Reformationstag]
+                }
+            }
         }
     }
 
@@ -121,69 +191,8 @@ const BUNDESWEITE_FEIERTAGE: &'static [GermanHoliday] = &[
     ErsterMai,
     ChristiHimmelfahrt,
     Pfingstmontag,
+    // https://www.gesetze-im-internet.de/einigvtr/art_2.html
     TagDerDeutschenEinheit,
     ErsterWeihnachtsfeiertag,
     ZweiterWeihnachtsfeiertag,
 ];
-
-#[cfg(test)]
-mod tests {
-    use crate::regions::GermanHoliday::*;
-    use crate::regions::GermanRegion;
-    use crate::regions::GermanRegion::*;
-    use crate::DateExt;
-    use chrono::NaiveDate;
-    use proptest::prelude::*;
-
-    #[test]
-    fn singular_example_holiday() {
-        let date = NaiveDate::from_ymd(2018, 1, 1);
-        assert!(date.is_public_holiday_in(Bayern));
-        assert_eq!(Some(Neujahr), date.public_holiday_in(Bayern));
-    }
-
-    proptest! {
-    #[test]
-    fn total_number_holidays(year in 2019i32..) {
-        let number_holidays = |region: GermanRegion| region.holidays_in_year(year).len();
-        assert_eq!(12, number_holidays(BadenWuerttemberg));
-        assert_eq!(13, number_holidays(Bayern));
-        assert_eq!(10, number_holidays(Berlin));
-        assert_eq!(10, number_holidays(Brandenburg));
-        assert_eq!(10, number_holidays(Bremen));
-        assert_eq!(10, number_holidays(Hamburg));
-        assert_eq!(10, number_holidays(Hessen));
-        assert_eq!(10, number_holidays(MechlenburgVorpommern));
-        assert_eq!(10, number_holidays(Niedersachsen));
-        assert_eq!(11, number_holidays(NordrheinWestfalen));
-        assert_eq!(11, number_holidays(RheinlandPfalz));
-        assert_eq!(12, number_holidays(Saarland));
-        assert_eq!(11, number_holidays(Sachsen));
-        assert_eq!(11, number_holidays(SachsenAnhalt));
-        assert_eq!(10, number_holidays(SchleswigHolstein));
-        assert_eq!(11, number_holidays(Thueringen));
-    }
-    }
-
-    #[test]
-    fn frauentag_in_berlin_since_2019() {
-        assert!(!Berlin.holidays_in_year(2018).contains(&Frauentag));
-        assert_eq!(
-            None,
-            NaiveDate::from_ymd(2018, 3, 8).public_holiday_in(Berlin)
-        );
-        assert!(Berlin.holidays_in_year(2019).contains(&Frauentag));
-        assert_eq!(
-            Some(Frauentag),
-            NaiveDate::from_ymd(2019, 3, 8).public_holiday_in(Berlin)
-        );
-    }
-
-    proptest! {
-    #[test]
-    fn only_provide_holidays_after_1995(year in -2999i32..1995) {
-        assert!(BadenWuerttemberg.holidays_in_year(year).is_empty());
-    }
-    }
-
-}
